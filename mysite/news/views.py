46254954dd081fbd2de73cf22347de80372b29dd
Nikita <58622735+developer-rni@users.noreply.github.com>
2021-03-23 19:05:41 +0300

    solved 13 lessons

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from .models import News
 
 
 def index(request):
-    # print(request)
-    return HttpResponse('Hello world')
-
-
-def test(request):
-    return HttpResponse('<h1>Тестовая страница</h1>')
+    news = News.objects.all()
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, template_name='news/index.html', context=context)
