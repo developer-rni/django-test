97d7cb94a63da221dce328781a207d31e38628aa
Nikita <58622735+developer-rni@users.noreply.github.com>
2021-03-25 17:42:25 +0300

    solved 24 lessons

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import News, Category
+from .forms import NewsForm
 
 
 def index(request):
@@ -29,3 +30,18 @@ def view_news(request, news_id):
         'news_item': news_item,
     }
     return render(request, 'news/view_news.html', context)
+
+
+def add_news(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            news = News.objects.create(**form.cleaned_data)
+            return redirect(news)
+    else:
+        form = NewsForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'news/add_news.html', context)
