86480ec8659af4027d40c215ef0a2f2adba220e9
Nikita <58622735+developer-rni@users.noreply.github.com>
2021-04-14 16:39:17 +0300

    solved 48 lessons

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -136,6 +136,11 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
+INTERNAL_IPS = ['127.0.0.1']
+
+EMAIL_HOST = ''  # 'smtp.mail.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = ''  # username@mail.ru
+EMAIL_HOST_PASSWORD = ''  # password
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
