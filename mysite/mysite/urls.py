19f3dcdbc66385c47091da722bba89519962363b
Nikita <58622735+developer-rni@users.noreply.github.com>
2021-04-15 15:00:59 +0300

    solved 49 lessons

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -8,6 +8,7 @@ from news.views import *
 urlpatterns = [
     path('', include('news.urls')),
     path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
 
 if settings.DEBUG:
